b735f865c665e8f6b168318479070641bf405adc
Pianto81 <90965863+Pianto81@users.noreply.github.com>
2024-10-06 19:23:52 +0200

    Ajout de Slash commmands

--- main.py
+++ main.py
@@ -2,39 +2,44 @@ import discord
 from discord.ext.commands import Bot
 import dotenv
 
-
-class MyClient(Bot):
-    async def on_ready(self):
-        print(f'Logged on as {self.user}!')
-
-    async def on_message(self, message):
-        channel = message.channel
-        mess = message.content.lower()
-        status = {"erreur":'🔴', "aide":'🟠', "cours":'🔵', "tests":'🟣', "fini":'🟢'}
-
-        if (mess[:2] == "p ") or (mess[:4] == "past"):
-            for pastille in status.keys():
-                if pastille in mess:
-                    print(f'{message.author} change la pastille de {channel.name} en {pastille}')
-
-                    if channel.name[0] in status.values():
-                        await channel.edit(name=f'{status[pastille]} - {channel.name[1:]}')
-
-                    else:
-                        await channel.edit(name=f'{status[pastille]} - {channel.name}')
-                    
-                    await channel.send(f"Pastille {status[pastille]} changée avec succès !", delete_after=3)
-                    await message.delete(delay=3)
-                    return None
-
-
 intents = discord.Intents.default()
 intents.message_content = True
 
-client = MyClient(command_prefix="past", intents=intents)
+
+
+client = Bot(command_prefix="past", 
+                  intents=intents, 
+                  description="Gère les pastilles pour clarifier l'avancement du projet",
+                  strip_after_prefix= True)
+
 game = discord.Game(name="gérer votre code de singes", 
                     assets={'large_image':'bocchi_chair', 'large_text':'hmmm'}
                     )
 client.activity = game
+
+@client.tree.command(
+    name="pastille",
+    description="Change le statut actuel du salon, ",
+    guild=discord.Object(905399047521697802),
+)
+async def set_pastille(interaction:discord.Interaction, statut:str):
+    '''Change le statut du salon. \n
+      Args:
+        statut: "erreur": 🔴, "aide": 🟠, "cours": 🔵, "tests": 🟣, "fini": 🟢 '''
+    channel = interaction.channel
+    couleurs = {"erreur":'🔴', "aide":'🟠', "cours":'🔵', "tests":'🟣', "fini":'🟢'}
+
+    await interaction.response.send_message(content=f"statut changée en {couleurs[statut]} !")
+    if channel.name[0] in couleurs.values():
+        await channel.edit(name=f'{couleurs[statut]}-{channel.name[1:]}')
+    else:
+        await channel.edit(name=f'{couleurs[statut]}-{channel.name}')
+
+
+@client.event
+async def on_ready():
+    await client.tree.sync(guild=discord.Object(905399047521697802))
+    # "erreur: 🔴, aide: 🟠, cours: 🔵, tests: 🟣, fini: 🟢"
+    print("Ready!")
 
 client.run(dotenv.get_key('.env','TOKEN'))
